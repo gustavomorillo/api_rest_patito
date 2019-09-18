c3eb36a87fd21b747ca6aa71169002803eaec001
Gustavo Morillo <gustavomorillo@gmail.com>
2019-09-18 19:34:14 -0400

    fix time middleware

--- app/Http/Middleware/checkTime.php
+++ app/Http/Middleware/checkTime.php
@@ -35,7 +35,7 @@ class checkTime
             // y menor o igual a 61200 segundos permitir // RANGO de 8AM a 5PM hora bogota.
 
 
-        if($time_seconds >= 8800 && $time_seconds <= 91200){
+        if($time_seconds >= 28800 && $time_seconds <= 61200){
             return $next($request);
         } else {
             return response()->json('Fuera de horario');
